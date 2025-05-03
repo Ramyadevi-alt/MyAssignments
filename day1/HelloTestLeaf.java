f00a479ae492057d6b0adc5b5fe0224574c83706
Ramyadevi-alt <devidhinesh1995@gmail.com>
2025-05-03 13:50:22 +0530

    Add files via upload
    week1.day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+	//logic1
+		System.out.print("Hello Ramya");
+		System.out.println(" Welcome To TestLeaf");
+		
+		//logic2
+		String str1 = "Hello Ramya";
+		String str2 = " Welcome To TestLeaf";
+		System.out.println(str1+" "+str2);
+		
+
+	}
+
+}
